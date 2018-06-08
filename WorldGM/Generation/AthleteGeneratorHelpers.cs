4cca142ea3ffad2ca2434e09bee4a702bf3b511f
kesac <kevin@turtlesort.com>
2018-06-08 00:59:39 -0600

    Improved athlete generation

--- WorldGM/Generation/AthleteGeneratorHelpers.cs
+++ WorldGM/Generation/AthleteGeneratorHelpers.cs
@@ -4,13 +4,31 @@ using System.Text;
 
 namespace WorldGM.Generation
 {
-    public static class AthleteGeneratorHelpers
+    public static class GenerationHelpers
     {
         // Lower and upper limits are both inclusive
-        public static int Between(this Random r, int lower, int upper)
+        public static int Between(this Random r, int lowerInclusive, int upperInclusive)
         {
-            return r.Next(upper - lower) + lower + 1;
+            return r.Next(1 + upperInclusive - lowerInclusive) + lowerInclusive;
         }
+
+        public static int NormalizedBetween(this Random r, int lower, int upper, int mean, int deviation)
+        {
+            // https://stackoverflow.com/questions/218060/random-gaussian-variables
+            double u1 = 1.0 - r.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0 - r.NextDouble();
+            double randStdNormal = System.Math.Sqrt(-2.0 * System.Math.Log(u1)) * System.Math.Sin(2.0 * System.Math.PI * u2); //random normal(0,1)
+            double randNormal = mean + deviation * randStdNormal; //random normal(mean,stdDev^2)
+
+            return Convert.ToInt32((randNormal * (upper - lower)) + lower);
+        }
+
+        public static bool OnChance(this Random r, double chance)
+        {
+            return r.NextDouble() < chance;
+        }
+
+        
 
     }
 }
--- WorldGM/Generation/BasicAthleteGenerator.cs
+++ WorldGM/Generation/BasicAthleteGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WorldGM.Math;
 
 namespace WorldGM.Generation
 {
@@ -18,80 +19,53 @@ namespace WorldGM.Generation
 
         public Athlete NextAthlete()
         {
-            var result = new Athlete()
+            var athlete = new Athlete()
             {
                 FamilyName = this.NameGenerator.NextFamilyName(),
-                Age = this.Random.Between(18, 40)
+                Age = this.Random.NormalizedBetween(18, 40, 26, 5),
+                Weight = this.Random.NormalizedBetween(120, 230, 180, 20), // Needs normalized distribution later
+                Height = this.Random.NormalizedBetween(155, 210, 174, 10)  // Needs normalized distribution later
             };
 
-            var positionRoll = this.Random.NextDouble();
-
-            if(positionRoll < 0.30)
+            var p = new WeightedCollection<AthletePosition>();
+            p.Add(AthletePosition.Forward, 30);
+            p.Add(AthletePosition.Midfield, 30);
+            p.Add(AthletePosition.Defense, 30);
+            p.Add(AthletePosition.Goalkeeper, 10);
+            athlete.Position = p.Next();
+            
+            var g = new WeightedCollection<AthleteGender>();
+            g.Add(AthleteGender.Female, 45);
+            g.Add(AthleteGender.Male, 45);
+            g.Add(AthleteGender.Other, 10);
+            athlete.Gender = g.Next();
+            
+            if (athlete.Gender == AthleteGender.Female && this.Random.OnChance(0.9))
             {
-                result.Position = AthletePosition.Forward;
+                athlete.FirstName = this.NameGenerator.NextFeminineName();
             }
-            else if(positionRoll < 0.6)
+            else if (athlete.Gender == AthleteGender.Male && this.Random.OnChance(0.9))
             {
-                result.Position = AthletePosition.Midfield;
-            }
-            else if (positionRoll < 0.9)
-            {
-                result.Position = AthletePosition.Defense;
-            }
-            else
-            {
-                result.Position = AthletePosition.Goalkeeper;
-            }
-
-            var genderRoll = this.Random.NextDouble();
-
-            if (genderRoll < 0.45)
-            {
-                result.Gender = AthleteGender.Female;
-
-                if(this.Random.NextDouble() < 0.9)
-                {
-                    result.FirstName = this.NameGenerator.NextFeminineName();
-                }
-                else
-                {
-                    result.FirstName = this.NameGenerator.NextUnisexName();
-                }
-                
-            }
-            else if (genderRoll < 0.90)
-            {
-                result.Gender = AthleteGender.Male;
-
-                if (this.Random.NextDouble() < 0.9)
-                {
-                    result.FirstName = this.NameGenerator.NextMasculineName();
-                }
-                else
-                {
-                    result.FirstName = this.NameGenerator.NextUnisexName();
-                }
-                
+                athlete.FirstName = this.NameGenerator.NextMasculineName();
             }
             else
             {
-                result.Gender = AthleteGender.Other;
-                var nameRoll = this.Random.NextDouble();
-                if (nameRoll < 0.33)
+                if (this.Random.OnChance(0.33))
                 {
-                    result.FirstName = this.NameGenerator.NextFeminineName();
+                    athlete.FirstName = this.NameGenerator.NextFeminineName();
                 }
-                else if(nameRoll < 0.66)
+                else if(this.Random.OnChance(0.33))
                 {
-                    result.FirstName = this.NameGenerator.NextMasculineName();
-                }
-                else
-                {
-                    result.FirstName = this.NameGenerator.NextUnisexName();
+                    athlete.FirstName = this.NameGenerator.NextMasculineName();
                 }
             }
 
-            return result;
+            if(string.IsNullOrEmpty(athlete.FirstName))
+            {
+                athlete.FirstName = this.NameGenerator.NextUnisexName();
+            }
+
+            return athlete;
         }
 
         public Athlete NextAthlete(AthletePosition position)
