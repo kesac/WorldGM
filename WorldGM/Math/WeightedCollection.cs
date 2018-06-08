d3b4c6f44ca555996abbb99cd2483587e72fab31
kesac <kevin@turtlesort.com>
2018-06-08 00:26:25 -0600

    Added classes for selecting elements in a collection with weighted probability

---
+++ WorldGM/Math/WeightedCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorldGM.Math
+{
+
+    public class WeightedCollection<T>
+    {
+        private Random Random { get; set; }
+        public List<WeightedElement<T>> Elements { get; set; }
+
+        public WeightedCollection()
+        {
+            this.Random = new Random();
+            this.Elements = new List<WeightedElement<T>>();
+        }
+
+        public void Add(T value, int weight)
+        {
+            this.Elements.Add(new WeightedElement<T>(value, weight));
+        }
+
+        public T Next()
+        {
+            int roll = this.Random.Next(this.Elements.Sum(x => x.Weight));
+            int chance = 0;
+
+            for(int i = 0; i < this.Elements.Count(); i++)
+            {
+                chance += this.Elements[i].Weight;
+                if (roll < chance)
+                {
+                    return this.Elements[i].Value;
+                }
+            }
+
+            throw new InvalidOperationException("No selection could be made because the collection is empty or because an element was removed while the collection was selecting an element");
+        }
+            
+    }
+}
---
+++ WorldGM/Math/WeightedElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldGM.Math
+{
+    public class WeightedElement<T>
+    {
+        public T Value { get; set; }
+        public int Weight { get; set; }
+
+        public WeightedElement(T value, int weight)
+        {
+            this.Value = value;
+            this.Weight = weight;
+        }
+    }
+}
