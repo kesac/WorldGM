1b0c875ab1d75df25710e5731c3ea7d6c44010b0
kesac <kesac@users.noreply.github.com>
2021-11-14 16:38:09 -0700

    Added Party class +Represents a group of Characters on an adventure

---
+++ WorldGM/Entities/Party.cs
@@ -0,0 +1,23 @@
+﻿using Loremaker;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WorldGM.Entities
+{
+    public class Party : Identifiable
+    {
+        public uint Id { get; set; }
+        public List<uint> CharacterIds { get; set; }
+
+        [IgnoreDataMember]
+        public virtual List<Character> Characters {get;set;}
+
+        public Party()
+        {
+            this.CharacterIds = new List<uint>();
+            this.Characters = new List<Character>();
+        }
+    }
+}
