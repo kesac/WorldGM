16611e874ffc1fe29375c523f4d9d2cedb0245d6
kesac <kesac@users.noreply.github.com>
2021-11-20 23:26:14 -0700

    Added new generators for characters and character attribute values

---
+++ WorldGM/Generation/CharacterGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Archigen;
+using Loremaker;
+using Loremaker.Names;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorldGM.Entities;
+
+namespace WorldGM.Generation
+{
+    public class CharacterGenerator : Generator<Character>
+    {
+        public CharacterGenerator() : this(new DefaultNameGenerator()) { }
+
+        public CharacterGenerator(IGenerator<string> nameGenerator)
+        {
+            this.ForProperty<string>(x => x.FirstName, nameGenerator)
+                .ForProperty<string>(x => x.FamilyName, nameGenerator)
+                .ForProperty<int>(x => x.Level, 1)
+                .ForProperty<int>(x => x.Strength, 1)
+                .ForProperty<int>(x => x.Intelligence, 1)
+                .ForProperty<int>(x => x.Dexterity, 1)
+                .ForProperty<int>(x => x.Resistance, 1)
+                .ForProperty<int>(x => x.Speed, 1)
+                .ForProperty<int>(x => x.Vitality, 1)
+                .ForProperty<CharacterAppearance>(x => x.Appearance, CharacterAppearance.Neutral)
+                .ForProperty<CharacterElement>(x => x.Element, CharacterElement.Fire);
+        }
+
+    }
+}
---
+++ WorldGM/Generation/NumberGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Archigen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldGM.Generation
+{
+    public class NumberGenerator : IGenerator<int>
+    {
+        private int Min;
+        private int Max;
+        private Random Random;
+
+        public NumberGenerator(int minimumInclusive, int maximumInclusive)
+        {
+            this.Min = minimumInclusive;
+            this.Max = maximumInclusive + 1;
+            this.Random = new Random();
+        }
+
+        public int Next()
+        {
+            return this.Random.Next(Min, Max);
+        }
+    }
+}
