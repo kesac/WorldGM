09239af45162f6476859baf0b6dd9b68e1a72614
kesac <kesac@users.noreply.github.com>
2021-11-21 20:29:59 -0700

    Basic models for character abilities

---
+++ WorldGM/Entities/Ability.cs
@@ -0,0 +1,37 @@
+﻿using Loremaker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldGM.Entities
+{
+
+    public class Ability : Identifiable
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public List<AbilityAction> Actions { get; set; }
+
+        public Ability()
+        {
+            this.Actions = new List<AbilityAction>();
+        }
+
+        public static readonly Ability DefaultAbility = new Ability()
+        {
+            Id = 0,
+            Name = "Single Strike",
+            Actions = new List<AbilityAction>()
+            {
+                new AbilityAction()
+                {
+                    Type = AbilityActionType.PhysicalAttack,
+                    TargetType = TargetType.EnemyRandom,
+                    // AttributeSource = CharacterAttribute.Strength,
+                    MinMultiplier = 1.00f,
+                    MaxMultiplier = 1.20f
+                }
+            }
+        };
+    }
+}
---
+++ WorldGM/Entities/AbilityAction.cs
@@ -0,0 +1,11 @@
+﻿namespace WorldGM.Entities
+{
+    public class AbilityAction
+    {
+        public AbilityActionType Type { get; set; }
+        public TargetType TargetType { get; set; }
+        // public CharacterAttribute AttributeSource { get; set; }
+        public float MinMultiplier { get; set; }
+        public float MaxMultiplier { get; set; }
+    }
+}
