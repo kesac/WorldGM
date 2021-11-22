6695818f166ee2b4c8d6d28ac0cae4b5e86d7067
kesac <kesac@users.noreply.github.com>
2021-11-21 20:30:37 -0700

    Characters now have at least one primary ability

--- WorldGM/Entities/Character.cs
+++ WorldGM/Entities/Character.cs
@@ -1,4 +1,5 @@
 ﻿using Loremaker;
+using System;
 using System.Runtime.Serialization;
 
 namespace WorldGM.Entities
@@ -23,6 +24,44 @@ namespace WorldGM.Entities
         public int Resistance { get; set; }    // Resistance to debuffs
         public int Speed { get; set; }         // Turn frequency
         public int Vitality { get; set; }     // HP
+
+        public Ability PrimaryAbility { get; set; }
+
+        public int Get(CharacterAttribute attribute)
+        {
+            if(attribute == CharacterAttribute.Level)
+            {
+                return this.Level;
+            }
+            else if (attribute == CharacterAttribute.Strength)
+            {
+                return this.Strength;
+            }
+            else if (attribute == CharacterAttribute.Intelligence)
+            {
+                return this.Intelligence;
+            }
+            else if (attribute == CharacterAttribute.Dexterity)
+            {
+                return this.Dexterity;
+            }
+            else if (attribute == CharacterAttribute.Resistance)
+            {
+                return this.Resistance;
+            }
+            else if (attribute == CharacterAttribute.Speed)
+            {
+                return this.Speed;
+            }
+            else if (attribute == CharacterAttribute.Vitality)
+            {
+                return this.Vitality;
+            }
+            else
+            {
+                throw new InvalidOperationException("Tried to access an attribute that is not used by characters.");
+            }
+        }
 
     }
 }
